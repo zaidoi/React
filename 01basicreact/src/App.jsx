a98b6080baf88c7118ed29e0c04d12bc766fd257
zaidoi <zaidbadgujar7@gmail.com>
2025-08-07 16:56:48 +0530

    Conditionals rendering

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,13 +1,30 @@
+function List(props) {
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
 
-
-function App() {
- 
+  if (props.animals.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
 
   return (
- 
-     <h1>Learning React Zaid</h1>
-   
-  )
+    <ul>
+      {props.animals.map((animal) => {
+        return <li key={animal}>{animal}</li>;
+      })}
+    </ul>
+  );
+}
+
+function App() {
+  const animals = [];
+
+  return (
+    <div>
+      <h1>Animals: </h1>
+      <List  />
+    </div>
+  );
 }
 
 export default App
