2030acb37d2742a9aef7689b5873955c529bc0c4
zaidoi <zaidbadgujar7@gmail.com>
2025-08-12 23:54:10 +0530

    Rendering with filter jsx

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -3,14 +3,33 @@
 
 
 function App() {
-  const people = [
-    'Creola Katherine Johnson: mathematician',
-    'Mario José Molina-Pasquel Henríquez: chemist',
-    'Mohammad Abdus Salam: physicist',
-    'Percy Lavon Julian: chemist',
-    'Subrahmanyan Chandrasekhar: astrophysicist'
-  ];
-  const listItems = people.map((person)=> <li>{person}</li>)
+  const people = [{
+    id: 0,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+  }, {
+    id: 1,
+    name: 'Mario José Molina-Pasquel Henríquez',
+    profession: 'chemist',
+  }, {
+    id: 2,
+    name: 'Mohammad Abdus Salam',
+    profession: 'physicist',
+  }, {
+    id: 3,
+    name: 'Percy Lavon Julian',
+    profession: 'chemist',  
+  }, {
+    id: 4,
+    name: 'Subrahmanyan Chandrasekhar',
+    profession: 'astrophysicist',
+  }];
+   const chemist = people.filter(person => person.profession === 'chemist')
+
+   const listItems = chemist.map(person => <li><p>
+    <b>{person.name}</b>
+    {" " + person.profession + " "}
+    </p></li>)
 
   return (
    <ul>{listItems}</ul>
