2f11e5d47fc001e2b63790eb576c0115a8e64a20
zaidoi <zaidbadgujar7@gmail.com>
2025-08-18 20:27:35 +0530

    type of useEffect worked learned

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,17 +1,41 @@
 
+import { useEffect, useState } from "react";
 import Button from "./Btn";
 
 
 function App() {
   
+const [resource,setResource] = useState('posts')
+useEffect(() =>{
+  console.log("clicked");
+  // works on dependencies
+},[resource])
 
+useEffect(() =>{
+  console.log("clicked");
+  // works only one time
+  
+},[])
+
+useEffect(() =>{
+// this prints after return this react useEffect clean up work which first
+// run return then the main fuction great yrr..
+  console.log("done");
+
+  return () =>{
+    console.log("clean up");
+    
+  }
+  
+})
 
   return (
-    <div>
-    <Button  />
-    <Button text="Don't Click Me!" color="red" fontSize={12} />
-    <Button  fontSize={20} />
-  </div>
+    <>
+    <button onClick={() => setResource("posts")}>Posts</button>
+    <button onClick={() => setResource("blog")}>blog</button>
+    <button onClick={() => setResource("comp")}>comp</button>
+    <p>{resource}</p>
+    </>
   );
 }
 
