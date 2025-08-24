3f0af8eef6c0707f228387db79384e450123f7f4
zaidoi <zaidbadgujar7@gmail.com>
2025-08-24 18:37:07 +0530

    Learned about test in React

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,40 +1,20 @@
 
-import { useEffect, useState } from "react";
+import {  useState } from "react";
+import PropTypes from 'prop-types';
+import RenderName from "./RenderName";
 import Button from "./Btn";
 
 
 function App() {
-  
-const [resource,setResource] = useState('posts')
-useEffect(() =>{
-  console.log("clicked");
-  // works on dependencies
-},[resource])
+  const [heading, setHeading] = useState("Magnificent Monkeys");
 
-useEffect(() =>{
-  console.log("clicked");
-  // works only one time
-  
-},[])
-
-useEffect(() =>{
-// this prints after return this react useEffect clean up work which first
-// run return then the main fuction great yrr..
-  console.log("done");
-
-  return () =>{
-    console.log("clean up");
-    
-  }
-  
-})
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
 
   return (
     <>
-    <button onClick={() => setResource("posts")}>Posts</button>
-    <button onClick={() => setResource("blog")}>blog</button>
-    <button onClick={() => setResource("comp")}>comp</button>
-    <p>{resource}</p>
+      <RenderName  />
     </>
   );
 }
