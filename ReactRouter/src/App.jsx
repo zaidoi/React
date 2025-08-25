8e18a6f35574c57a728a47d760f0c952b18cba9c
zaidoi <zaidbadgujar7@gmail.com>
2025-08-25 16:10:42 +0530

    Made Header Home and Footer and used RouterProvider

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
+import Home from "./components/Home/Home"
+import Footer from './components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    <h1 className='bg-green-500'>React Router</h1>
+   
       
     </>
   )
