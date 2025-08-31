87e1eef15ab816ae6c7a5b18445e4cceeeaf8991
zaidoi <zaidbadgujar7@gmail.com>
2025-08-31 23:31:33 +0530

    Mini Context Done

--- OdinReactRouter/src/DefaultProfile/DefaultProfile.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DefaultProfile() {
-  return (
-    <p>Oh, nothing to see here!</p>
-  )
-}
-
-export default DefaultProfile
--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -2,7 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
@@ -30,19 +35,15 @@ import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout/>}>
-      <Route path="" element={<Home/>}/>
-      <Route path="about" element={<About/>}/>
-      <Route path="contact" element={<Contact/>}/>
-      <Route
-      loader={githubInfoLoader}
-       path="github"
-        element={<Github/>} />
-      <Route path="user/:id" element={<User/>}/>
-
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<About />} />
+      <Route path="home" element={<Home />} />
+      <Route path="contact" element={<Contact />} />
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
+      <Route path="user/:id" element={<User />} />
     </Route>
   )
-)
+);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
