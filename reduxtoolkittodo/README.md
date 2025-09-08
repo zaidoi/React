367a7d7c2c256800e532c758a66ca791060d2fbd
zaidoi <zaidbadgujar7@gmail.com>
2025-09-08 23:48:54 +0530

    Reducer Made

--- reduxtoolkittodo/README.md
+++ reduxtoolkittodo/README.md
@@ -10,3 +10,11 @@ Currently, two official plugins are available:
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+
+steps redux
+
+1.ConfigureStore bring in the store.js
+2.Now create a slice file 
+3.and go to slice file and then createSlice with name and initial state and reducers
+4.now the reducers with property always gives state and action as parameter
---
+++ reduxtoolkittodo/src/features/todo/todoSlice.js
@@ -0,0 +1,31 @@
+import {createSlice, nanoid} from '@reduxjs/toolkit'
+
+const initialState = {
+    todos:[{id:1 , text:"Hello world"}]
+}
+
+export const todoSlice = createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+    addTodo:(state,action) => {
+        const todo = {
+            id:nanoid(),
+            text:action.payload.text
+        }
+        state.todos.push(todo)
+    },
+    removeTodo:(state,action) => {
+    state.todos = state.todos.filter(todo => todo.id !== action.payload) 
+    },
+    updateTodo:(state,action) => {
+    const newVal = action.payload.text
+    state.todos = state.todos.map((todo) =>{
+        if(todo.id == action.payload.id){
+        return {...todo,text:newVal};
+        }
+        return todo
+    })
+    }
+    }
+})
