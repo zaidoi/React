57a24c9a637e1a792ad0839b1830d67048a6b845
zaidoi <zaidbadgujar7@gmail.com>
2025-09-25 23:30:39 +0530

    Learnt new Hook useReducer

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,14 +1,26 @@
-function UncontrolledExample() {
-  const inputRef = React.useRef();
+import { useActionState, useReducer } from "react";
 
-  const handleSubmit = () => {
-    console.log(inputRef.current.value); // get value from DOM
-  };
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      state;
+  }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   return (
-    <>
-      <input type="text" ref={inputRef} defaultValue="John Doe" />
-      <button onClick={handleSubmit}>Log Value</button>
-    </>
+    <div>
+      <p>Count:{state.count}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>Incre</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decre</button>
+    </div>
   );
 }
+
+export default Counter;
