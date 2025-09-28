804610baae1be934ece64dce9aa90a9484bbf6f5
zaidoi <zaidbadgujar7@gmail.com>
2025-09-28 21:35:59 +0530

    Build a Search Bar from freecodecamp practice callback and memo

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,23 +1,79 @@
-import { useOptimistic } from "react";
+const { useState, useMemo, useCallback } = React;
 
-function LikeButton({ postId }) {
-  const [optimisticLikes, addOptimisticLike] = useOptimistic(
-    0, // initial likes
-    (state, newLike) => state + newLike // how to update
-  );
+const items = [
+  "Apples",
+  "Bananas",
+  "Strawberries",
+  "Blueberries",
+  "Mangoes",
+  "Pineapple",
+  "Lettuce",
+  "Broccoli",
+  "Paper Towels",
+  "Dish Soap",
+];
 
-  async function handleLike() {
-    // instantly update UI
-    addOptimisticLike(1);
+let prevToggleItem = null;
 
-    // then actually call server
-    try {
-      await fetch(`/api/like/${postId}`, { method: "POST" });
-    } catch {
-      // rollback if server fails
-      addOptimisticLike(-1);
-    }
+export const ShoppingList = () => {
+  const [query, setQuery] = useState("");
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const filteredItems = useMemo(() => {
+    console.log("Filtering items...");
+    return items.filter((item) =>
+      item.toLowerCase().includes(query.toLowerCase())
+    );
+  }, [query]);
+
+  const toggleItem = useCallback((item)=>{
+    setSelectedItems((prev) =>
+      prev.includes(item) ? prev.filter((i) => i !== item) : [...prev, item]
+    );
+  },[setSelectedItems])
+
+  if (prevToggleItem !== toggleItem) {
+    console.log("New toggleItem function");
+    prevToggleItem = toggleItem;
+  } else {
+    console.log("Current toggleItem function");
   }
 
-  return <button onClick={handleLike}>👍 {optimisticLikes}</button>;
-}
+  return (
+    <div className="container">
+      <h1>Shopping List</h1>
+      <form>
+        <label htmlFor="search">Search for an item:</label>
+        <input
+          id="search"
+          type="search"
+          placeholder="Search..."
+          aria-describedby="search-description"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        /> 
+        <p id="search-description">Type to filter the list below:</p>
+        <ul>
+          {filteredItems.map((item) => {
+            const isChecked = selectedItems.includes(item);
+            return (
+              <li
+                key={item}
+                style={{ textDecoration: isChecked ? "line-through" : "none" }}
+              >
+                <label>
+                  <input
+                    type="checkbox"
+                    onChange={() => toggleItem(item)}
+                    checked={isChecked}
+                  />
+                  {item}
+                </label>
+              </li>
+            );
+          })}
+        </ul>
+      </form>
+    </div>
+  );
+};
