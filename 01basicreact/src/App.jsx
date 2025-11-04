c5540d03264e2f5a43596640fc7d35a9d77fb53e
zaidoi <zaidbadgujar7@gmail.com>
2025-11-04 23:45:53 +0530

    Progress Bar made

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,68 +1,56 @@
-import React, { useCallback, useState } from 'react';
-import './styles.css'
+import React, { useState } from 'react';
 
-function ContactForm() {
-    const [form, setFormData] = useState({ name: "", email: "", message: "" })
-    const [submit, setSubmit] = useState(true)
-    const [errors,setErrors] = useState({})
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const validationState = validate()
-        if (Object.keys(validationState).length > 0) {
-            setErrors(validationState)
-        } else {
-        setSubmit(!submit)
-        }
+function ProgressBar() {
+
+    const [progress, setProgress] = useState(0)
+    const updateProgress = (val) => {
+        setProgress(prev => Math.max(0,Math.min(100,prev+val)))
     }
-    const validate = () => {
-        const Errors = {}
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!form.name.trim()) Errors.name = "Name is required"
-        if (!form.email.trim()) {
-            Errors.email = "Email is required"
-        } else if (!emailRegex.test(form.email)) {
-            Errors.email = "Invalid email format"
-        }
-        if(!form.message.trim()) Errors.message = 'Message is required'
-        return Errors
-        
-    }
-    const resetForm = () => {
-        setSubmit(!submit)
-        setFormData({ name: "", email: "", message: "" })
-    }
+    const colorChange = () => {
+        if (progress >= 80) return 'green'
+        if (progress >= 40) return 'orange'
+        return 'red'
+}
+
     return (
-        <div >
-            {submit ? <form onSubmit={handleSubmit}>
-                <label>Name: </label>
-                <input
-                    required
-                    type='text'
-                    value={form.name}
-                    onChange={(e) => setFormData(prev => ({ ...prev, name: e.target.value }))}
-                />
-                {errors.name && <p>{errors.name}</p>}
-                <label>Email: </label>
-                <input
-                    required
-                    type='text'
-                    value={form.email}
-                    onChange={(e) => setFormData(prev => ({ ...prev, email: e.target.value }))}
-                />
-                {errors.email && <p>{errors.email}</p>}
-                <label>Message:</label>
-                <textarea
-                    required
-                    type='text'
-                    value={form.message}
-                    onChange={(e) => setFormData(prev => ({ ...prev, message: e.target.value }))} />
-                <button type='submit'>Submit</button>
-            </form> : <><
-                h1>"Thank you, {form.name}"</h1>
-                <button onClick={()=>resetForm()}>Reset Form</button>
-            </>}
+        <div>
+            <h2>Progress Bar</h2>
+            <div class="outer" style={{
+                border: "1px solid black",
+                textAlign: "center",
+                borderRadius: "5px",
+                position: 'relative',
+                height:'20px'
+            
+            }}>
+                <div class="inner"
+                    style={{
+                        width: `${progress}%`,
+                        backgroundColor: colorChange(),
+                        height: "100%" ,
+                        transition:'width 0.3s ease-in-out'
+                }}
+                ></div>
+                <span style={{
+                    position: "absolute",
+                    left: '50%',
+                    top: '0%',
+                    transform: "translateX(-50%)"
+
+                }}>{progress}%</span>
+            </div>
+            <div style={{
+        
+                margin: "5px",
+                display:"flex",
+                gap:"5px"
+            }}>
+                <button onClick={()=>updateProgress(-10)}>-10</button>
+                <button onClick={()=>updateProgress(+10)}>+10</button>
+            </div>
+            
         </div>
     );
 }
 
-export default ContactForm;
+export default ProgressBar;
